847d87eebc083e6ab6e3199f1aa2adf92009b2d0
ellenrheiner <ellen.reiner.santos@escola.pr.gov.br>
2025-10-28 08:18:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const avanca = document.querySelectorAll('.btm-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
-        cont atual = document.querySelector('.ativo');
-    
+        const atual = document.querySelector('.ativo');
+         const proximoPasso = 'passo-' + this.getAttribute(data-proximo);
+         
+         atual.classList.remove('ativo');
+         document.getElementById(proximoPasso).classList.add('ativo');
      })
 })
